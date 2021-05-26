5f77a8d8465d895b6c29064b427eb2f93d8000c5
NrdyBhu1 <NrdyBhu1@gmail.com>
2021-05-26 17:15:50 +0530

    I had a cup of tea and now it's fixed

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+extern crate glium;
+
+fn main() {
+    // 1. The **winit::EventsLoop** for handling events.
+    let mut events_loop = glium::glutin::event_loop::EventLoop::new();
+    // 2. Parameters for building the Window.
+    let wb = glium::glutin::window::WindowBuilder::new()
+        .with_inner_size(glium::glutin::dpi::LogicalSize::new(1024.0, 768.0))
+        .with_title("Hello world");
+    // 3. Parameters for building the OpenGL context.
+    let cb = glium::glutin::ContextBuilder::new();
+    // 4. Build the Display with the given window and OpenGL context parameters and register the
+    //    window with the events_loop.
+    let display = glium::Display::new(wb, cb, &events_loop).unwrap();
+}
