2b79d4fb9cc143c492907c73afa97e7ed2db6ec6
NrdyBhu1 <NrdyBhu1@gmail.com>
2021-06-18 13:49:09 +0530

    Finished glium book tutorials

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 extern crate glium;
+extern crate image;
+
 use glium::{
     glutin::{
         self,
@@ -9,10 +11,12 @@ use glium::{
     },
     implement_vertex,
     index::NoIndices,
-    Display, Surface, VertexBuffer,
+    texture::{RawImage2d, Texture2d},
+    uniform, Display, Surface, VertexBuffer,
 };
 use std::{
     fs::read,
+    io::Cursor,
     path::Path,
     time::{Duration, Instant},
 };
@@ -25,12 +29,12 @@ pub fn read_file_as_string(pt: &Path) -> Result<String, &'static str> {
 #[derive(Debug, Copy, Clone)]
 struct Vertex {
     position: [f32; 2],
+    tex_coords: [f32; 2],
 }
 
 fn main() {
-    implement_vertex!(Vertex, position);
+    implement_vertex!(Vertex, position, tex_coords);
     let mut frame: f32 = -0.5;
-
 
     // window and event loops
     let el: EventLoop<()> = glutin::event_loop::EventLoop::new();
@@ -38,9 +42,42 @@ fn main() {
     let cb: ContextBuilder<NotCurrent> = ContextBuilder::new();
     let display: Display = Display::new(wb, cb, &el).unwrap();
 
+    // Tetures
+    let wink_raw_rgba8 = image::load(
+        Cursor::new(&include_bytes!("../assets/tex/wink-side.png")),
+        image::ImageFormat::Png,
+    )
+    .unwrap()
+    .to_rgba8();
+    let wink_dimensions = wink_raw_rgba8.dimensions();
+    let wink = RawImage2d::from_raw_rgba_reversed(&wink_raw_rgba8.into_raw(), wink_dimensions);
+    let wink_texture: Texture2d = Texture2d::new(&display, wink).unwrap();
+    println!("{:?}", wink_dimensions);
+    println!("{}, {}", wink_dimensions.0 / 10, wink_dimensions.1 / 10);
+
     // shaders
-    let vertex_shader_src: String = read_file_as_string(Path::new("./default.vert")).unwrap();
-    let fragment_shader_src: String = read_file_as_string(Path::new("./default.frag")).unwrap();
+    let vertex_shader_src: String =
+        read_file_as_string(Path::new("./assets/shaders/default.vert")).unwrap();
+    let fragment_shader_src: String =
+        read_file_as_string(Path::new("./assets/shaders/default.frag")).unwrap();
+
+    // triangle
+    let v1: Vertex = Vertex {
+        position: [-0.5, -0.5],
+        tex_coords: [0.0, 0.0],
+    };
+    let v2: Vertex = Vertex {
+        position: [0.0, 0.5],
+        tex_coords: [1.0, 0.0],
+    };
+    let v3: Vertex = Vertex {
+        position: [0.5, -0.5],
+        tex_coords: [0.0, 1.0],
+    };
+    let shape: Vec<Vertex> = vec![v1, v2, v3];
+
+    // upload to vertex buffer
+    let vertex_buffer: VertexBuffer<Vertex> = VertexBuffer::new(&display, &shape).unwrap();
 
     // create program
     let program = glium::Program::from_source(
@@ -66,26 +103,22 @@ fn main() {
             },
             _ => (),
         }
-        // triangle
-        let v1: Vertex = Vertex {
-            position: [-0.5+frame, -0.5],
-        };
-        let v2: Vertex = Vertex {
-            position: [0.0+frame, 0.5],
-        };
-        let v3: Vertex = Vertex {
-            position: [0.5+frame, -0.5],
-        };
-        let shape: Vec<Vertex> = vec![v1, v2, v3];
-
-        // upload to vertex buffer
-        let vertex_buffer: VertexBuffer<Vertex> = VertexBuffer::new(&display, &shape).unwrap();
 
         frame += 0.0002;
 
-        if frame > 0.5 {
+        if frame > 0.7 {
             frame = -0.5;
         }
+
+        let uniforms = uniform! {
+            matrix: [
+                [1.0, 0.0, 0.0, 0.0],
+                [0.0, 1.0, 0.0, 0.0],
+                [0.0, 0.0, 1.0, 0.0],
+                [frame, 0.0, 0.0, 1.0f32],
+            ],
+            tex: &wink_texture
+        };
 
         // start drawing
         let mut target = display.draw();
@@ -97,7 +130,7 @@ fn main() {
                 &vertex_buffer,
                 &index_buffer,
                 &program,
-                &glium::uniforms::EmptyUniforms,
+                &uniforms,
                 &Default::default(),
             )
             .unwrap();
